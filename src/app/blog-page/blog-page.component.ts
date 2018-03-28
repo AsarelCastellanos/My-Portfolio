9f2416cf9bd6e310808b1a33ca9b8dfca0218dab
Asarel Castellanos <asarelcastellanos@asarels-mbp.lan>
2018-03-27 20:54:08 -0700

    Can now pull comments based on ids

--- src/app/blog-page/blog-page.component.ts
+++ src/app/blog-page/blog-page.component.ts
@@ -5,6 +5,7 @@ import { BlogService } from '../_services/blog/blog.service';
 import { Router } from '@angular/router';
 import { baseUrl } from '../_services/httpBaseUrl/httpBaseUrl';
 import { CommentService } from '../_services/comment/comment.service';
+import { error } from 'util';
 
 class Blog {
   author: String;
@@ -52,6 +53,7 @@ interface ServerResponse {
 }
 
 
+
 @Component({
   selector: 'app-blog-page',
   templateUrl: './blog-page.component.html',
@@ -60,6 +62,7 @@ interface ServerResponse {
 
 export class BlogPageComponent implements OnInit {
   public comment: Comment;
+  public newComment: Comment;
   public blog: Blog;
   public headerID;
   public cycleComments;
@@ -67,12 +70,21 @@ export class BlogPageComponent implements OnInit {
   constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient, private commentService: CommentService) {
     this.route.params.subscribe((params) => {
       this.headerID = params.id;
+      this.pullData();
       console.log(params);
     });
   }
 
   ngOnInit() {
+
+  }
+
+  pullData() {
+
+    const headers = new HttpHeaders().set('ID', this.headerID);
+
     this.blog = new Blog;
+    // HTTP Get Blogs
     this.http.get(baseUrl + '/postBlog', {
       headers: new HttpHeaders().set('headerID', this.headerID)
     })
@@ -80,21 +92,25 @@ export class BlogPageComponent implements OnInit {
         this.blog = data;
         console.log(this.blog);
       });
-      this.http.get(baseUrl + '/postComments')
+    // HTTP Get Comments
+    this.http.get(baseUrl + '/postComment', { headers })
       .subscribe((data: CommentInterface) => {
         this.comment = data;
         this.cycleComments = this.comment;
         console.log(this.comment);
       });
-      this.comment = new Comment;
+    this.comment = new Comment;
+    this.newComment = new Comment;
   }
-
   onSubmit() {
-    this.comment.discussionId = this.headerID;
-    console.log(this.comment);
-    this.commentService.comment(this.comment)
+    this.newComment.discussionId = this.headerID;
+    console.log(this.newComment);
+    this.commentService.comment(this.newComment)
       .subscribe((res: ServerResponse) => {
+        if (error) {
+          throw error;
+        }
         console.log(res);
       });
-    }
+  }
 }
